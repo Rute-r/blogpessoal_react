65a8488631a4e55ec85b4f937515833d8d485ff4
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-15 14:19:00 -0300

    Adicionando Tailwind ao projeto e adicionando estilo ao componente Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,14 @@
 function Home() {
   return (
     <>
-      <div
-        style={{
-          width: '100vw',
-          display: 'flex',
-          justifyContent: 'center',
-        }}>
+      <div className="w-screen flex justify-center">
         <div>
-          <div
-            style={{
-              width: '80vw',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}>
+          <div className="mt-5 max-w-7xl flex flex-col items-center">
             <h2>Seja Bem Vinde!</h2>
             <p>Expresse aqui seus pensamentos e opniões</p>
           </div>
 
-          <div
-            style={{
-              width: '80vw',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}>
+          <div className="mt-5 max-w-7xl flex flex-col items-center">
             <img src="https://imgur.com/VpwApCU.png" alt="Imagem de Pagina Home" width="400px" />
           </div>
         </div>
