5e973a19c4d442e0bbfa0039cf3b2799bf9b242e
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-15 15:30:01 -0300

    adicionando estilo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 function Navbar() {
   return (
     <>
-      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+      <div className="w-full flex justify-center py-4 bg-cyan-900 text-pink-200">
         <div className="container flex justify-between text-lg">
           Blog Pessoal
-          <div className="flex gap-4">Postagens Temas Cadastrar tema Perfil Sair</div>
+          <div className="flex gap-4">Postagens | Temas | Cadastrar tema | Perfil | Sair</div>
         </div>
       </div>
     </>
