61e9ae4530ddb7cbde02af6a682860dbb76ac385
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-15 15:29:44 -0300

    Adicionando estilo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,14 @@
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react';
 
 function Footer() {
+  // eslint-disable-next-line prefer-const
   let data = new Date().getFullYear();
 
   return (
     <>
-      <div className="flex justify-center bg-indigo-900 text-white">
+      <div className="flex justify-center bg-cyan-900 text-pink-200">
         <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">Blog Pessoal Generation | Copyright: {data}</p>
+          <p className="text-xl font-bold">Blog Pessoal Rute | Copyright: {data}</p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
             <LinkedinLogo size={48} weight="bold" />
