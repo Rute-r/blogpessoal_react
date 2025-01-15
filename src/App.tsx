fb8786f5598299dc51eb6c1da2881000b6c13355
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-15 15:06:11 -0300

    adicionando navbar e footer ao App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
