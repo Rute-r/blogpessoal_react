8136de9dc406c3ab4665465fa1211ce263bf03d4
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-16 10:46:25 -0300

    Adicionando a Pagina Login.tsx e Login.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home/Home';
 import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} /> */}
-          <Route path="/" element={<Cadastro />} />
-        </Routes>
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+
         <Footer />
       </BrowserRouter>
     </>
